07937a775f17957a678b26a2838c6321c9eebd88
DESKTOP-7S3VVJS\yasse <yesser.tahri@gmail.com>
2022-12-16 13:00:19 +0100

    edit Constants class

--- src/main/java/com/company/tondeuse/tranverse/Constants.java
+++ src/main/java/com/company/tondeuse/tranverse/Constants.java
@@ -2,12 +2,12 @@ package com.company.tondeuse.tranverse;
 
 public final class Constants {
 
-    public static final String file = "src/main/resources/file.txt";
-    public static final String SEPARTOR = " ";
-    public static final String CR_SEPARTOR = "\n";
-    public static final String sourcePattern = "^\\d+ \\d+$";
-    public static final String SOURCE_PATTERN_MOWER = "^\\d+ \\d+ [N|E|W|S]$";
-    public static final String BAD_DATA = "Problem of data gived !";
-    public static final String BAD_DIRECTION = "Bad direction !";
+    public final static String file = "src/main/resources/file.txt";
+    public final static String SEPARTOR = " ";
+    public final static String CR_SEPARTOR = "\n";
+    public final static String sourcePattern = "^\\d+ \\d+$";
+    public final static String SOURCE_PATTERN_MOWER = "^\\d+ \\d+ [N|E|W|S]$";
+    public final static String BAD_DATA = "Problem of data gived !";
+    public final static String BAD_DIRECTION = "Bad direction !";
     public static final String BAD_POSITION = "Bad position !";
 }
